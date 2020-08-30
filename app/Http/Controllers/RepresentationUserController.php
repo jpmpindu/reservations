02bc66a861ea07498f157da3479762cc7ac5e853
jpmpindu <jpmpindu@gmail.com>
2020-08-30 20:20:40 +0200

    creation of model, controller, migration, seeder, factories

---
+++ app/Http/Controllers/RepresentationUserController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\RepresentationUser;
+use Illuminate\Http\Request;
+
+class RepresentationUserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\RepresentationUser  $representationUser
+     * @return \Illuminate\Http\Response
+     */
+    public function show(RepresentationUser $representationUser)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\RepresentationUser  $representationUser
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(RepresentationUser $representationUser)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\RepresentationUser  $representationUser
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, RepresentationUser $representationUser)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\RepresentationUser  $representationUser
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(RepresentationUser $representationUser)
+    {
+        //
+    }
+}
