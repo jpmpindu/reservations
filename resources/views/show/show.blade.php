be7b21cc9dec7d2204f6aeeadc5e2003abd4d816
jpmpindu <jpmpindu@gmail.com>
2020-09-01 08:24:27 +0200

    Templates Show: affichage des représentations

--- resources/views/show/show.blade.php
+++ resources/views/show/show.blade.php
@@ -23,5 +23,15 @@
         @else
         <p><em>Non réservable</em></p>
         @endif
+        
+        <h2>Liste des représentations</h2>
+        @if($show->representations->count()>=1)
+        <ul>
+            @foreach ($show->representations as $representation)
+                <li>{{ $representation->when }}</li>              
+            @endforeach
+        </ul>
+        @else
+        <p>Aucune représentation</p>
+        @endif
 @endsection
-
