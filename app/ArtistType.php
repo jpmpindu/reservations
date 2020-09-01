60ca9016d3f363b46c875ae1ca18178c7fca8b9b
jpmpindu <jpmpindu@gmail.com>
2020-09-01 08:38:39 +0200

    Relation ManyToMany between ArtistType & Show

--- app/ArtistType.php
+++ app/ArtistType.php
@@ -6,5 +6,41 @@ use Illuminate\Database\Eloquent\Model;
 
 class ArtistType extends Model
 {
-    //
+        protected $fillable = [
+        'artist_id', 'type_id',
+    ];
+
+    protected $table = 'artist_type';
+    public $timestamps = false;
+    
+    /**
+     * Get the show of the performance (artist in a type of collaboration for a show)
+     */
+    public function show()
+    {
+        return $this->belongsToMany('App\Show');
+    }
+
+    /**
+     * Get the artist for that association.
+     */
+    public function artist()
+    {
+        return $this->belongsTo('App\Artist');
+    }
+    
+    /**
+     * Get the type for that association.
+     */
+    public function type()
+    {
+        return $this->belongsTo('App\Type');
+    }
+
+        public function artistType()
+    {
+        return $this->belongsTo('App\ArtistType');
+    }
+
+
 }
